61ece8f279ca4b155d0801a885d5ad7ad84633df
JOTAPEPA <juanpereira.liza@gmail.com>
2024-10-10 09:39:10 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import {Quasar} from 'quasar'
-
-
+import {Quasar, Notify} from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import App from './App.vue'
@@ -9,7 +7,9 @@ import App from './App.vue'
 const myApp = createApp(App)
 
 myApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
+  plugins: {
+  Notify
+  }, // import Quasar plugins and add here
 })
 
 // Assumes you have a <div id="app"></div> in your index.html
